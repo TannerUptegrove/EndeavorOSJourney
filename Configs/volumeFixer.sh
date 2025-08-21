8285d11cdc6757cbe93f6a94805ea4c1bd6ae6cf
Tanner Uptegrove <tanneruptegrove@gmail.com>
2025-08-20 22:38:37 -0600

    Added a bash script to prevent the volume from exceeding 100%

---
+++ Configs/volumeFixer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+volume=$(pactl list sinks | grep Volume | head -n1 | awk '{print $5}')
+
+if [ $volume != 100% ]; then 
+    pactl set-sink-volume \@DEFAULT_SINK@ +5%
+    echo "here"  
+fi
