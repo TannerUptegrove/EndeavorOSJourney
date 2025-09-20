98aee469263053daa9ef95e5bae46580bdb82601
TannerUptegrove <tanneruptegrove@gmail.com>
2025-09-20 14:40:05 -0500

    Edited Config Files and added Pictures. Changed Config Files and added Pictures.

--- Configs/Sway/config.sh
+++ Configs/Sway/config.sh
@@ -1,163 +1,153 @@
-### Variables
-#
-# Mod4 is the Windows Key. Use Mod1 for alt. 
+# Includes the default configuration for sway.
+include /etc/sway/config.d/*
+
+# ========== Variables ================================================
+# Mod 4 is the Windows key - used to activate the sway keybinds.
 set $mod Mod4
 
-# Defining default apps
+# Default apps saved into variables.
 set $term kitty
-set $menu pgrep wofi >/dev/null 2>&1 && killall wofi || wofi --show drun
+set $menu pgrep rofi >/dev/null 2>&1 && killall rofi || rofi -show drun
 set $file_explorer nautilus
 set $browser vivaldi
-set $Discord discord
-set $vscode code-oss
-set $steam steam
+# =====================================================================
 
-# Default wallpaper (more resolutions are available in /usr/share/backgrounds/sway/)
-output * bg /home/dev/Pictures/Wallpapers/RainworldForest.png fill
+
+
+# ========== Background Configuration =================================
+# Use the command "swaymsg -t get_outputs" to get the monitor name.
 output eDP-1 resolution 1920x1080 position 0,0
+output * bg /home/dev/Pictures/Wallpapers/RainworldForest.png fill
+# =====================================================================
 
-### Sway Customization
-# Gaps
+
+
+# ========== Sway Look ================================================
 gaps inner 6px
 gaps outer 2px
-
-# Border Size
 default_border pixel 1px
 default_floating_border pixel 1Vpx
 
-# Border Color Variables
+# Border Colors
 set $bdr_high #92a565
 set $bdr_back #526b34
 set $bdr_urge #30a17a
-
-# Border Colors
 client.focused          $bdr_high #141216 $bdr_high $bdr_high $bdr_high 
 client.focused_inactive $bdr_back #141216 $bdr_high $bdr_back $bdr_back
 client.unfocused        $bdr_back #141216 $bdr_high $bdr_back $bdr_back
 client.urgent           $bdr_urge #141216 $bdr_high $bdr_urge $bdr_urge
+# =====================================================================
 
-# Enables natural scrolling from the touchpad
+
+
+# ========== Mouse Configurations =====================================
+# These configure both the touchpad and my mouse.
+
 input 1160:4122:DELL0A20:00_0488:101A_Touchpad {
-    dwt enabled
-    tap enabled
-    natural_scroll enabled
-    middle_emulation enabled
+  dwt enabled                # This enables "disabled while typing"
+  tap enabled                # Enables tap clicking
+  natural_scroll enabled     # Enables inverted scrolling.
+  middle_emulation enabled
 }
 
-#Mouse Settings
 input 1133:49291:Logitech_G502_HERO_Gaming_Mouse {
-    dwt enabled
-    tap enabled
-    natural_scroll disabled
-    pointer_accel -0.7 # set mouse sensitivity (between -1 and 1)
+  dwt enabled
+  natural_scroll disabled
+  pointer_accel -0.7  # Sets the sensitivity (can be between -1 and 1).
 }
+# =====================================================================
 
-### Key bindings
-#
-# Basics:
-#
-    bindsym $mod+Return exec $term
-    bindsym $mod+e exec $file_explorer
-    bindsym $mod+a exec $menu
-    bindsym $mod+s exec $browser
-    bindsym $mod+d exec $Discord;
-    bindsym $mod+c exec $vscode
-    bindsym $mod+x exec steam
-    bindsym $mod+q kill
 
-    # Reload the configuration file
-    bindsym $mod+Shift+c reload
+# ========== Key Bindings =============================================
+# Reloads the system.
+bindsym $mod+Shift+c reload
 
-    bindsym $mod+k exec sway-screenshot -m output -o /home/dev/Pictures/Screenshots/
-#
-# Moving around:
-#
-    # Or use $mod+[up|down|left|right]
-    bindsym $mod+Left focus left
-    bindsym $mod+Down focus down
-    bindsym $mod+Up focus up
-    bindsym $mod+Right focus right
+# Opens the default apps.
+bindsym $mod+Return exec $term
+bindsym $mod+e exec $file_explorer
+bindsym $mod+a exec $menu
+bindsym $mod+s exec $browser
+bindsym $mod+Shift+s exec $browser -incognito
+bindsym $mod+d exec discord;
+bindsym $mod+c exec code-oss
+bindsym $mod+x exec steam
+bindsym $mod+q kill
+    
+# Uses the sway-sceeenshot package to take sceeenshots.
+bindsym $mod+k exec sway-screenshot -m output -o /home/dev/Pictures/Screenshots/
 
-    # Ditto, with arrow keys
-    bindsym $mod+Shift+Left move left
-    bindsym $mod+Shift+Down move down
-    bindsym $mod+Shift+Up move up
-    bindsym $mod+Shift+Right move right
-#
-# Workspaces:
-#
-    # Switch to workspace
-    bindsym $mod+1 workspace number 1
-    bindsym $mod+2 workspace number 2
-    bindsym $mod+3 workspace number 3
-    bindsym $mod+4 workspace number 4
-    bindsym $mod+5 workspace number 5
-    bindsym $mod+6 workspace number 6
-    bindsym $mod+7 workspace number 7
-    bindsym $mod+8 workspace number 8
-    bindsym $mod+9 workspace number 9
-    bindsym $mod+0 workspace number 10
-    # Move focused container to workspace
-    bindsym $mod+Shift+1 move container to workspace number 1
-    bindsym $mod+Shift+2 move container to workspace number 2
-    bindsym $mod+Shift+3 move container to workspace number 3
-    bindsym $mod+Shift+4 move container to workspace number 4
-    bindsym $mod+Shift+5 move container to workspace number 5
-    bindsym $mod+Shift+6 move container to workspace number 6
-    bindsym $mod+Shift+7 move container to workspace number 7
-    bindsym $mod+Shift+8 move container to workspace number 8
-    bindsym $mod+Shift+9 move container to workspace number 9
-    bindsym $mod+Shift+0 move container to workspace number 10
-#
-# Layout stuff:
-#
-    # Make the current focus fullscreen
-    bindsym $mod+f fullscreen
+# Use to change the focus to another app if multiple are open.
+bindsym $mod+Left focus left
+bindsym $mod+Right focus right
+bindsym $mod+Down focus down
+bindsym $mod+Up focus up
 
-    # Toggle the current focus between tiling and floating mode
-    bindsym $mod+Shift+space floating toggle
+# Used to change where an app is on the screen.
+bindsym $mod+Shift+Left move left
+bindsym $mod+Shift+Down move down
+bindsym $mod+Shift+Up move up
+bindsym $mod+Shift+Right move right
 
-    # Swap focus between the tiling area and the floating area
-    bindsym $mod+space focus mode_toggle
+# Used to switch between the workspaces.
+bindsym $mod+1 workspace number 1
+bindsym $mod+2 workspace number 2
+bindsym $mod+3 workspace number 3
+bindsym $mod+4 workspace number 4
+bindsym $mod+5 workspace number 5
+bindsym $mod+6 workspace number 6
+bindsym $mod+7 workspace number 7
+bindsym $mod+8 workspace number 8
+bindsym $mod+9 workspace number 9
+bindsym $mod+0 workspace number 10
 
-    # Drag floating windows by holding down $mod and left mouse button.
-    floating_modifier $mod normal
-#
-# Scratchpad:
-#
-    # Sway has a "scratchpad", which is a bag of holding for windows.
-    # You can send windows there and get them back later.
+# Used to move the current focused app to another workspace.
+bindsym $mod+Shift+1 move container to workspace number 1
+bindsym $mod+Shift+2 move container to workspace number 2
+bindsym $mod+Shift+3 move container to workspace number 3
+bindsym $mod+Shift+4 move container to workspace number 4
+bindsym $mod+Shift+5 move container to workspace number 5
+bindsym $mod+Shift+6 move container to workspace number 6
+bindsym $mod+Shift+7 move container to workspace number 7
+bindsym $mod+Shift+8 move container to workspace number 8
+bindsym $mod+Shift+9 move container to workspace number 9
+bindsym $mod+Shift+0 move container to workspace number 10
+    
+# Puts the current focused app in fullscreen.
+bindsym $mod+f fullscreen
 
-    # Move the currently focused window to the scratchpad
-    bindsym $mod+Shift+minus move scratchpad
+# Puts the current focused app in floating mode.
+# It can then be moved around with the mouse.
+bindsym $mod+Shift+space floating toggle
 
-    # Show the next scratchpad window or hide the focused scratchpad window.
-    # If there are multiple scratchpad windows, this command cycles through them.
-    bindsym $mod+minus scratchpad show
-#
-# Utilities:
-#
-    # Special keys to adjust volume via PulseAudio
-    bindsym XF86AudioMute exec pactl set-sink-mute \@DEFAULT_SINK@ toggle
-    bindsym XF86AudioLowerVolume exec pactl set-sink-volume \@DEFAULT_SINK@ -5%
+# Swaps focus from the tiling area to the floating area.
+bindsym $mod+space focus mode_toggle
 
-    #Will run an bash script that will check if the volume is 100%, if it isn't, then the volume will be raised.
-    bindsym XF86AudioRaiseVolume exec .config/sway/volumeFixer.sh
-    bindsym XF86AudioMicMute exec pactl set-source-mute \@DEFAULT_SOURCE@ toggle
+# Drags floating windows by holding down $mod and left mouse button.
+floating_modifier $mod normal
 
-    # Special keys to adjust brightness via brightnessctl
-    bindsym --locked XF86MonBrightnessDown exec brightnessctl set 10%-
-    bindsym --locked XF86MonBrightnessUp exec brightnessctl set 10%+
-    # Special key to take a screenshot with grim
-    bindsym Print exec grim
 
-#
-# Status Bar:
-#
-# Read `man 5 sway-bar` for more information about this section.
+# Sway has a scatchpad which can store windows.
+# Used to send windows into the scratchpad.
+bindsym $mod+Shift+minus move scratchpad
+
+# Used to show the scratchpad
+bindsym $mod+minus scratchpad show
+
+# Keybinding used to adjust the audio.
+bindsym XF86AudioMute exec pactl set-sink-mute \@DEFAULT_SINK@ toggle
+bindsym XF86AudioLowerVolume exec pactl set-sink-volume \@DEFAULT_SINK@ -5%
+bindsym XF86AudioRaiseVolume exec .config/sway/volumeFixer.sh #Will run the VolumeFixer script.
+bindsym XF86AudioMicMute exec pactl set-source-mute \@DEFAULT_SOURCE@ toggle
+
+# Keybindings used to adjust the brightness.
+bindsym --locked XF86MonBrightnessDown exec brightnessctl set 10%-
+bindsym --locked XF86MonBrightnessUp exec brightnessctl set 10%+
+# =====================================================================
+
+
+# ========== Taskbar ==================================================
+# Used to put a waybar taskbar on sway
 bar {
     swaybar_command waybar
 }
-
-
-include /etc/sway/config.d/*
+# =====================================================================
--- Configs/Waybar/config.sh
+++ Configs/Waybar/config.sh
@@ -1,74 +1,75 @@
 {
-    "layer": "bot",
-    "position": "top",
-    "spacing": 5,
-    "height": 0,
+  "layer": "bot",
+  "position": "top",
+  "spacing": 5,
+  "height": 0,
 
-    "modules-left": [ "clock", "cpu", "memory", "network" ],
-    "modules-center": [ "sway/workspaces" ],
-    "modules-right": [ "pulseaudio", "battery", "custom/power"],
+  "modules-left": [ "clock", "cpu", "memory", "network" ],
+  "modules-center": [ "sway/workspaces" ],
+  "modules-right": [ "pulseaudio", "battery", "custom/power"],
 
-    "sway/workspaces": {
-        "format": "",
-        "persistent-workspaces": {
-            "1": [],
-            "2": [],
-            "3": [],
-            "4": []
-        },
-
-        "disable-scroll": true,
-        "disable-click": false,
-        "tooltip": false
+  "sway/workspaces": {
+    "format": "",
+    "persistent-workspaces": {
+      "1": [],
+      "2": [],
+      "3": [],
+      "4": []
     },
 
-    "clock": {
-        "format": "{:%I:%M %D}",
-        "tooltip": false
+    "disable-scroll": true,
+    "disable-click": false,
+    "tooltip": false
+  },
+
+  "clock": {
+    "format": "{:%I:%M %D}",
+    "tooltip": false
+  },
+
+  "cpu": {
+    "format": "CPU: {usage}%",
+    "tooltip": false
+  },
+
+  "memory": {
+    "format": "RAM: {}%",
+    "tooltip": false
+  },
+
+  "network": {
+    "format": "NET: {bandwidthTotalBits}",
+    "format-disconnected": "No Network",
+    "interval": 5,
+    "on-click": "exec nm-connection-editor",
+    "tooltip": false
+  },
+
+  "pulseaudio": {
+    "scroll-step": 1,
+    "format": "VOL: {volume}%",
+    "format-muted": "VOL: Mute",
+    "nospacing": 1,
+    "on-click": "pavucontrol",
+    "tooltip": false
+  },
+
+  "battery": {
+    "states": {
+      "warning": 15,
+      "critical": 10
     },
 
-    "cpu": {
-        "format": "CPU: {usage}%",
-        "tooltip": false
-    },
+    "interval": 1,
+    "format": "BAT: {capacity}%",
+    "format-charging": "BAT: +{capacity}%",
+    "onclick": "print(Bat)",
+    "tooltip": false
+  },
 
-    "memory": {
-        "format": "RAM: {}%",
-        "tooltip": false
-    },
-
-    "network": {
-        "format": "NET: {bandwidthTotalBits}",
-        "format-disconnected": "No Network",
-        "interval": 5,
-        "on-click": "exec nm-connection-editor",
-        "tooltip": false
-    },
-
-    "pulseaudio": {
-        "scroll-step": 1,
-        "format": "VOL: {volume}%",
-        "format-muted": "VOL: Mute",
-        "nospacing": 1,
-        "on-click": "pavucontrol",
-        "tooltip": false
-    },
-
-    "battery": {
-        "states": {
-            "warning": 15,
-            "critical": 10
-        },
-
-        "interval": 1,
-        "format": "BAT: {capacity}%",
-        "format-alt": "BAT: {time}",
-        "tooltip": false
-    },
-
-    "custom/power": {
-        "format": "",
-        "on-click": "pgrep eww && killall eww || bash ~/.config/eww/Scripts/powerMenu.sh",
-        "tool-tip": false
-    }
+  "custom/power": {
+    "format": "",
+    "on-click": "pgrep eww && killall eww || bash ~/.config/eww/Scripts/powerMenu.sh",
+    "tooltip": false
+  }
 }
